76db08001af24a371c786094db22c4ee4301f638
Pedro Cattete <pedro.cattete@gmail.com>
2025-08-16 13:34:47 -0300

    Create Resource Lumber.py

---
+++ Resource Lumber.py
@@ -0,0 +1,123 @@
+Journal.Clear()
+while not Journal.Search("There's not enough wood here to harvest."):
+
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3277)
+    Misc.Pause(100)
+              
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3280)
+    Misc.Pause(100)
+          
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3283)
+    Misc.Pause(100)
+            
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3284)
+    Misc.Pause(100)
+    
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3286)
+    Misc.Pause(100)
+        
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3288)
+    Misc.Pause(100)
+            
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3289)
+    Misc.Pause(100)
+    
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3290)
+    Misc.Pause(100)
+    
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3291)
+    Misc.Pause(100)
+ 
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3293)
+    Misc.Pause(100)
+     
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3299)
+    Misc.Pause(100)
+        
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3300)
+    Misc.Pause(100)
+    
+    playerX = Player.Position.X    
+    playerY = Player.Position.Y - 1
+    playerZ = Player.Position.Z
+    axe = Items.FindByID(0x0F43,-1,Player.Backpack.Serial)
+    Items.UseItem(axe.Serial)
+    Target.WaitForTarget(10000, False)
+    Target.TargetExecute(playerX, playerY, playerZ, 3302)
+    Misc.Pause(100)
+    
+    Misc.SendMessage("!!!!!!!!!!Rodou!!!!!!!!!!")
+else:
+    Misc.Pause(3000)
